28a762877f62ed4e272159e9230a3180e1697a69
오동웅 <woong@MacPro.local>
2021-05-30 21:59:17 +0900

    Add to res, req

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,9 @@ import express from "express";
 const PORT: number = 1779;
 
 const app = express();
+app.get("/", (req, res) => {
+  return res.end();
+});
 
 app.listen(PORT, (): void => {
   console.log(`Hello server for http://localhost:${PORT} port🚀`);
