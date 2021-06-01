e71449ef823eee2bc3dca6dde1f348e0008f7100
오동웅 <woong@MacPro.local>
2021-06-01 22:46:26 +0900

    Install the morgan @type

--- dist/server.js
+++ dist/server.js
@@ -4,10 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
 const PORT = 1779;
 const app = express_1.default();
+const logger = morgan_1.default("dev");
+app.use(logger);
 app.get("/", (req, res) => {
-    return res.end();
+    console.log(`Please show your any ${req}`);
+    res.send(`<section><h1>Done!</h1><h2>Check your console</h2></section>`);
 });
 app.listen(PORT, () => {
     console.log(`Hello server for http://localhost:${PORT} port🚀`);
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT: number = 1779;
-
 const app = express();
-app.get("/", (req, res) => {
-  return res.end();
+const logger = morgan("dev");
+
+app.use(logger);
+app.get("/", (req, res): void => {
+  console.log(`Please show your any ${req}`);
+  res.send(`<section><h1>Done!</h1><h2>Check your console</h2></section>`);
 });
 
 app.listen(PORT, (): void => {
